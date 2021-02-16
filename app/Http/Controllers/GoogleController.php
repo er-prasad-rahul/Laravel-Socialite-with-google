a5a05ac9d471d2572433b3303ff3c732a1db1214
er-rahul-prasad <rahul.trinityinfosystem@gmail.com>
2021-02-16 10:36:34 +0530

    initial commit

---
+++ app/Http/Controllers/GoogleController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+use Socialite;
+use Auth;
+use Exception;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class GoogleController extends Controller
+{
+    public function redirectToGoogle()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+
+    public function handleGoogleCallback()
+    {
+        try {
+
+            $user = Socialite::driver('google')->user();
+
+            $finduser = User::where('google_id', $user->id)->first();
+
+            if($finduser){
+
+                Auth::login($finduser);
+
+                return redirect('/home');
+
+            }else{
+                $newUser = User::create([
+                    'name' => $user->name,
+                    'email' => $user->email,
+                    'google_id'=> $user->id,
+                    'password' => encrypt('123456dummy')
+                ]);
+
+                Auth::login($newUser);
+                return redirect('/home');
+              }
+
+            }      catch (Exception $e) {
+                dd($e->getMessage());
+        }
+    }
+
+}
---
+++ app/Models/User.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable
+{
+    use HasFactory, Notifiable;
+
+
+    protected $fillable = [
+        'name', 'email','password','google_id'
+    ];
+
+
+    protected $hidden = ['password', 'remember_token',];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+}
